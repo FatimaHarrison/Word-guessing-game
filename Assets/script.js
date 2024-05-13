c46b4ad0c843598088cce48c11043e4684117c82
FatimaHarrison <106970872+FatimaHarrison@users.noreply.github.com>
2024-05-13 10:35:04 -0700

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,90 +1,61 @@
-var words = ["lamborghini", "disney", "iphone", "missippi", "ruby", "pink",
-"orlando", "digital", "train", "quinceanera", "washington", "christmas",
-"teammate", "lie", "sabado", "marcales", "yahoo", "watermelon",
-"javascript", "ticket", "egg", "internet", "browser", "walmart", "generation",
-"patrick", "galaxy", "cat", "kitten", "ice", "bracelet", "incentive", "highschool", "windows", "juno"];
 
-var wins = 0, losses = 0, numGuesses = 26, count = 0;
-var wordIndex = 0;
-var displayWord = [], wrongGuesses = [];
-var duplicate;
+var words = ["disney", "pink","orlando",  "washington", "yahoo", "watermelon", "walmart",
+"patrick", "cat", "kitten", "windows", "juno", "marcales"];
 
-resetGame();
-console.log(words[wordIndex]);
-document.getElementById("word").innerHTML = displayMysteryWord();
-document.onkeyup = function (event) {
-    var input = event.key;
-    var regExp = new RegExp(input, "gi");
-    var placeholder = words[wordIndex];
-      if(placeholder.search(/[a-z]|[A-Z]/) != -1){
-        if(placeholder.includes(input)){
-            duplicate = placeholder.match(regExp);
-            for(var i = 0; i < duplicate.length; i++){
-                displayWord[placeholder.indexOf(input)] = input;
-                placeholder = placeholder.replace(input, " ");
-           }
-        }
-        else{
-            if(document.getElementById("wrongLetters").innerHTML.includes(input.toLowerCase(),14)){
+var array = [
+        { q: "A baby elephant weighs about 2,055 pounds.", a: "disney" },
+        { q: "Micheal Lawrence was a screenwritter.", a: "yahoo" },
+        { q: "The Marine Corps birthday is Nov 10th, 1775.", a: "cat" },
+        { q: "Chrispother Columbus discovered Egypt.", a: "marcales" },
+        { q: "Canada was the 2nd country to gain their indepence.", a: "juno" },
+        { q: "Gelato ice cream is the same as regular ice cream.", a: "patrick" },
+        { q: "Moana is the 56th Disney movie.", a: "kitten" },
+        { q: "Everybody hates Chris.", a: "walmart" },
+        { q: "Peter Pan is the 14th Disney moive.", a: "pink" },
+        { q: "Call of duty is a fun game.", a: "orlando" },
+        { q: "Ruby is a part of coding.", a: "washington" },
+        { q: "Janet Jackson is a silbing out of 5.", a: "windows" },
+        { q: "We all have our ups and downs.", a: "apple" }
+    ];
 
+    document.getElementById("question").innerHTML = array[count].q;
+    document.onkeyup   = function (event) {
+        var input = event.key;
+        if(input == words){
+            if (array[count].a == input) {
+                alert("Correct");
+                score++;
+                document.getElementById("score").innerHTML = "Current score: " + score;
             }
             else{
-                numGuesses--;
-                wrongGuesses.push(input.toLowerCase());
-                document.getElementById("wrongLetters").innerHTML = "Wrong letters: " + wrongGuesses;
-                var temp = "star" + starNum;
-                starNum++;
-                
+                alert("Incorrect");
+                score--;
+                       
+            }
+            count++;
+            if(count < 10){
+                document.getElementById("question").innerHTML = array[count].q;
+            }
+            else{
+                alert("You've reached to the end");
             }
         }
-    }
-
-    updateGame();
-}
-
-function updateGame(){
-    document.getElementById("word").innerHTML = displayMysteryWord();
-    setTimeout
-    if(numGuesses <= 0){
-        losses++
-        alert("Sorry game over!");
-        resetGame();
-    }
-    if(!(document.getElementById("word").innerHTML.includes("_"))){
-        if(numGuesses > 0){
-            wins++;
-            alert("Congratulations you've won!");
-            resetGame();
+        else{
+            alert("Remember the letters are all lowercase.");
         }
     }
-    document.getElementById("word").innerHTML = displayMysteryWord();
-    document.getElementById("Wins").innerHTML = "Wins: " + wins;
-    document.getElementById("Losses").innerHTML = "Losses: " + losses;
-    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + numGuesses;
-}
 
-function resetGame(){
-    numGuesses = 26;
-    wrongGuesses = [], displayWord = [];
-    document.getElementById("wrongLetters").innerHTML = "Wrong letters: " + wrongGuesses;
-    wordIndex = Math.floor(Math.random()*words.length);
-    for(var i = 0; i < words[wordIndex].length; i++){
-        displayWord.push(" _ ");
+   
+    function displayTutorial() {
+            var x = document.getElementById("tutorial");
+            var y = document.getElementById("tutorialButton");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+                y.textContent = "Put away instructions";
+            } else {
+                x.style.display = "none";
+                y.textContent = "Reveal instructions";
+            }
     }
-    for(var i = 1; i <= 10; i++){
-        var temp = "star" + i;
-        
-    }
-    starNum = 1;
-}
-
-function displayMysteryWord(){
-    var output = "";
-    for(var i = 0; i < displayWord.length; i++){
-        output += displayWord[i];
-    }
-    return output;
-}
-
-
-        
+    
+   
