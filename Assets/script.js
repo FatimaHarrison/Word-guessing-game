b84503b77df24e2f563c1a45d3fccff56dc88856
FatimaHarrison <106970872+FatimaHarrison@users.noreply.github.com>
2024-05-13 09:40:29 -0700

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,23 @@ var words = ["lamborghini", "disney", "iphone", "missippi", "ruby", "pink",
 "orlando", "digital", "train", "quinceanera", "washington", "christmas",
 "teammate", "lie", "sabado", "marcales", "yahoo", "watermelon",
 "javascript", "ticket", "egg", "internet", "browser", "walmart", "generation",
-"patrick", "galaxy", "z", "cat", "kitten", "ice", "bracelet", "incentive", "highschool", "windows", "juno"];
+"patrick", "galaxy", "cat", "kitten", "ice", "bracelet", "incentive", "highschool", "windows", "juno"];
+
+var array = [
+    { q: "A baby elephant weighs about 2,055 pounds.", a: "f" },
+    { q: "Micheal Lawrence was a screenwritter.", a: "t" },
+    { q: "The Marine Corps birthday is Nov 10th, 1775.", a: "t" },
+    { q: "Chrispother Columbus discovered Egypt.", a: "f" },
+    { q: "Canada was the 2nd country to gain their indepence.", a: "f" },
+    { q: "Gelato ice cream is the same as regular ice cream.", a: "f" },
+    { q: "Moana is the 56th Disney movie.", a: "t" },
+    { q: "Everybody hates Chris.", a: "f" },
+    { q: "Peter Pan is the 14th Disney moive.", a: "t" },
+    { q: "Call of duty is a fun game.", a: "t" },
+    { q: "Ruby is a part of coding.", a: "t" },
+    { q: "Janet Jackson is a silbing out of 5.", a: "f" },
+    { q: "We all have our ups and downs.", a: "t" }
+];
 
 var wins = 0, losses = 0, numGuesses = 26, count = 0;
 var wordIndex = 0;
@@ -12,7 +28,7 @@ var duplicate;
 resetGame();
 console.log(words[wordIndex]);
 document.getElementById("word").innerHTML = displayMysteryWord();
-
+document.getElementById("question").innerHTML = array[count].q;
 document.onkeyup = function (event) {
     var input = event.key;
     var regExp = new RegExp(input, "gi");
@@ -86,3 +102,6 @@ function displayMysteryWord(){
     }
     return output;
 }
+
+
+        
